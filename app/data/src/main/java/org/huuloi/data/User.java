f968a60df0722f1c8bcd27fb957c13de38ee4a2d
Hoi Dan It with Eric <leminhtris0102@gmail.com>
2024-03-17 12:06:12 +0700

    Sqlite

---
+++ app/data/src/main/java/org/huuloi/data/User.java
@@ -0,0 +1,14 @@
+package org.huuloi.data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
